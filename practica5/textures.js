73ba3bfedee06e7fa7162dcdfdb9a6440aadd827
Alejandro Granados <agranados@heligrafics.net>
2021-10-19 12:24:39 +0200

    f

--- practica5/textures.js
+++ practica5/textures.js
@@ -1,4 +1,4 @@
-const steel_texture = new THREE.TextureLoader().load("images/metal_128x128.jpg")
+const steel_texture = new THREE.TextureLoader().load("./images/metal_128x128.jpg")
 
 steel = new THREE.MeshPhongMaterial({
     map: steel_texture,
